e5bd5b404ab91fb3ef7f5fec4eb02a66ea479fd3
Dmitry <zguchiy@gmail.com>
2016-04-24 21:18:47 +0300

    crud

---
+++ models/Article.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content'], 'required'],
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+        ];
+    }
+}
